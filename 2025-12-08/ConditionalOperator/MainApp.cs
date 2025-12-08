b9596cc1c3c06f8b6ea5401e04ad2e419f9626a7
LEEINAH <22na7592@gmail.com>
2025-12-08 10:13:22 +0900

    ConditionalOperator

---
+++ 2025-12-08/ConditionalOperator/MainApp.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConditionalOperator
+{
+	class MainApp
+	{
+		static void Main(string[] args)
+		{
+			// 10 % 2 를 해서 값이 0이 나오는게 true면 : 기준 왼쪽에 있는 것을 출력 false면 오른쪽에 있는 것을 출력
+			string result = (10 % 2) == 0 ? "짝수" : "홀수"; // 짝수
+
+			Console.WriteLine(result);
+		}
+	}
+}
