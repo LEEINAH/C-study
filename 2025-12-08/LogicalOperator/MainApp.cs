2e4e4bfad489065c747d543799a38d32e872bcf6
LEEINAH <22na7592@gmail.com>
2025-12-08 10:09:16 +0900

    LogicalOperator

---
+++ 2025-12-08/LogicalOperator/MainApp.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace LogicalOperator
+{
+	class MainApp
+	{
+		static void Main(string[] args)
+		{
+			// &&은 둘 다 true여야 true가 됨.
+			Console.WriteLine("Testing && ...");
+			Console.WriteLine($"1 > 0 && 4 < 5 : {1 > 0 && 4 < 5}"); // True
+			Console.WriteLine($"1 > 0 && 4 > 5 : {1 > 0 && 4 > 5}"); // False
+			Console.WriteLine($"1 == 0 && 4 > 5 : {1 == 0 && 4 > 5}"); // False
+			Console.WriteLine($"1 == 0 && 4 < 5 : {1 == 0 && 4 < 5}"); // False
+
+			// ||은 둘 중 하나만 true여도 true가 나옴.
+			Console.WriteLine("\nTesting || ...");
+			Console.WriteLine($"1 > 0 || 4 < 5 : {1 > 0 || 4 < 5}"); // True
+			Console.WriteLine($"1 > 0 || 4 > 5 : {1 > 0 || 4 > 5}"); // True
+			Console.WriteLine($"1 == 0 || 4 > 5 : {1 == 0 || 4 > 5}"); // False
+			Console.WriteLine($"1 == 0 || 4 < 5 : {1 == 0 || 4 < 5}"); // True
+
+			// !는 값을 반대로 바꿔줌.
+			Console.WriteLine("\nTesting ! ...");
+			Console.WriteLine($"!True : {!true}"); // False
+			Console.WriteLine($"!False : {!false}"); // True
+		}
+	}
+}
