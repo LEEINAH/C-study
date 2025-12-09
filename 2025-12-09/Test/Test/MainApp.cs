e96f0093ab3d2f43ba11fd408e8eea4d5bc063a0
LEEINAH <22na7592@gmail.com>
2025-12-10 08:33:33 +0900

    Test

---
+++ 2025-12-09/Test/Test/MainApp.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Test
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 5; i > 0; i--)
+            {
+                for (int j = 5; j > 0; j--)
+                {
+                    if (j == i || j < i)
+                    {
+                        Console.Write("*");
+                    }                    
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
