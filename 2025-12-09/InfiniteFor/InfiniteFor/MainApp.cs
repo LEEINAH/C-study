95f3ccd7269d539cd6b846b4813976dacff886b6
LEEINAH <22na7592@gmail.com>
2025-12-09 16:22:10 +0900

    InfiniteFor

---
+++ 2025-12-09/InfiniteFor/InfiniteFor/MainApp.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace InfiniteFor
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            // for의 세 부분(초기화, 조건, 증감) 모두 생략되었기 때문에 무한루프가 돈다.
+            for (; ; )
+                Console.WriteLine(i++);
+        }
+    }
+}
