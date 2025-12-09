7edeace391ff232b3afd9ac04e68169a3d6f29bf
LEEINAH <22na7592@gmail.com>
2025-12-09 15:41:13 +0900

    DoWhile

---
+++ 2025-12-09/DoWhile/DoWhile/MainApp.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace DoWhile
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            int i = 10;
+
+            do
+            {
+                Console.WriteLine("a) i : {0}", i--);
+            }
+            while (i > 0);
+
+            do
+            {
+                Console.WriteLine("b) i : {0}", i--); // 여기서 i는 이미 0이지만 한 차례 더 실행.
+            }
+            while (i > 0);
+        }
+    }
+}
