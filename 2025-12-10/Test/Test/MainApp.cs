b36a28cb5369a4c59e1d36ad371f459b91738bf5
LEEINAH <22na7592@gmail.com>
2025-12-10 14:23:39 +0900

    Test

---
+++ 2025-12-10/Test/Test/MainApp.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Test
+{
+    class MainApp
+    {
+        static double Square(double number)
+        {
+            number = number * number;
+            return number;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write("수를 입력하세요 : ");
+
+            string input = Console.ReadLine();
+            double number = Convert.ToDouble(input);
+
+            Console.WriteLine($"결과 : {Square(number)}");
+        }
+    }
+}
