ed74c44a79f02349e24ec7cf5bc2c149ea1a6da4
LEEINAH <22na7592@gmail.com>
2025-12-10 14:30:13 +0900

    Test2

---
+++ 2025-12-10/Test2/Test2/MainApp.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Test2
+{
+    class MainApp
+    {
+        public static void Main()
+        {
+            double mean = 0;
+
+            Console.WriteLine("평균 : {0}", Mean(1, 2, 3, 4, 5, mean));
+        }
+
+        public static double Mean (
+            double a, double b, double c,
+            double d, double e, double mean)
+        {
+            mean = (a + b + c + d + e) / 5;
+            return mean;
+        }
+    }
+}
