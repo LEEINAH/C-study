91300c417f493951b27c21dbbd6634bfc0fd9809
LEEINAH <22na7592@gmail.com>
2025-12-11 08:55:16 +0900

    SealedMethod

---
+++ 2025-12-11/SealedMethod/MainApp.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Base
+{
+    public virtual void SealMe()
+    {
+
+    }
+}
+
+class Derived : Base 11 {
+    public sealed override void SealMe()
+    {
+        
+    }
+}
+
+class WantToOverride : Derived
+{
+    public override void SealMe()
+    {
+
+    }
+}
+
+class MainApp
+{
+    static void Main(string[] args)
+    {
+
+    }
+}
