c422b1c38beba9b290b9e6523ebae2bff84e3976
LEEINAH <22na7592@gmail.com>
2025-12-11 10:33:30 +0900

    AbstractClass

---
+++ 2025-12-11/AbstractClass/MainApp.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace AbstractClass
+{
+    abstract class AbstractBase // 불완전한 클래스, 객체로 직접 생성될 수 없고 반드시 다른 클래스에 의해 상속받아 사용돼야 함.
+    {
+        protected void PrivateMethodA()
+        {
+            Console.WriteLine("AbstractBase.PrivateMethodA()");
+        }
+
+        public void PublicMethodA()
+        {
+            Console.WriteLine("AbstractBase.PublicMethodA()");
+        }
+
+        public abstract void AbstractMethodA();
+    }
+
+    class Derived : AbstractBase
+    {
+        public override void AbstractMethodA()
+        {
+            Console.WriteLine("Derived.AbstractMethodA()");
+            PrivateMethodA();
+        }
+    }
+
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            AbstractBase obj = new Derived();
+            obj.AbstractMethodA();
+            obj.PublicMethodA();
+        }
+    }
+}
