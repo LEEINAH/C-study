da94348162b5bcd92f2e14352951782827c1f8a8
LEEINAH <22na7592@gmail.com>
2025-12-11 11:08:02 +0900

    AutoImplementedProperty

---
+++ 2025-12-11/AutoImplementedProperty/MainApp.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace AutoImplementedProperty
+{
+    class BirthdayInfo
+    {
+        public string Name { get; set; } = "Unknown";
+        public DateTime Birthday { get; set; } = new DateTime(1, 1, 1);
+        public int Age
+        {
+            get
+            {
+                // 생년월일에서 현재 연도를 사용해 나이 계산
+                return new DateTime(DateTime.Now.Subtract(Birthday).Ticks).Year;
+            }
+        }
+    }
+
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            BirthdayInfo birth = new BirthdayInfo();
+            Console.WriteLine($"Name : {birth.Name}");
+
+            // ToShortDateString()는 날짜를 짧은 형식의 문자열로 변환하여 반환해줌. (0000-00-00)
+            Console.WriteLine($"Birthday : {birth.Birthday.ToShortDateString()}");
+
+            Console.WriteLine($"Age : {birth.Age}");
+
+            birth.Name = "서현";
+            birth.Birthday = new DateTime(1991, 6, 28);
+
+            Console.WriteLine($"Name : {birth.Name}");
+            Console.WriteLine($"Birthday : {birth.Birthday.ToShortDateString()}");
+            Console.WriteLine($"Age : {birth.Age}");
+        }
+    }
+}
