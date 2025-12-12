4cf37b76671381f7ead00e6a0f19ede0ded2f751
LEEINAH <22na7592@gmail.com>
2025-12-12 16:13:25 +0900

    Ex12_1

---
+++ 2025-12-12/Ex12_1/MainApp.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Ex12_1
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[10];
+
+            for (int i = 0; i < 10; i++)
+                arr[i] = i;
+
+            try
+            {
+                for (int i = 0; i < 11; i++)
+                    Console.WriteLine(arr[i]);
+            }
+            catch (IndexOutOfRangeException e)
+            {
+                Console.WriteLine($"예외가 발생했습니다 : {e.Message}");
+            }
+
+            Console.WriteLine("종료");
+        }
+    }
+}
