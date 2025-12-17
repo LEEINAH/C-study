b95aca61da0f79cbbcd2134ef3ce4bfb9ff9c3ea
LEEINAH <22na7592@gmail.com>
2025-12-17 09:09:26 +0900

    Test

---
+++ 2025-12-17/Test/MainApp.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+
+namespace Test
+{
+    class Car
+    {
+        public int Cost { get; set; }
+        public int MaxSpeed { get; set; }
+    }
+
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            Car[] cars =
+            {
+                new Car() {Cost = 56, MaxSpeed = 120 },
+                new Car() {Cost = 70, MaxSpeed = 150 },
+                new Car() {Cost = 45, MaxSpeed = 180 },
+                new Car() {Cost = 32, MaxSpeed = 200 },
+                new Car() {Cost = 82, MaxSpeed = 280 }
+            };
+
+            var selected = cars
+                           .Where (car => car.Cost >= 50 && car.MaxSpeed >= 150)
+                           .Select (car => new
+                           {
+                               Cost = car.Cost,
+                               MaxSpeed = car.MaxSpeed
+                           });
+
+            foreach (var car in selected) 
+                Console.WriteLine($"Cost : {car.Cost}, MaxSpeed : {car.MaxSpeed}");
+        }
+    }
+}
