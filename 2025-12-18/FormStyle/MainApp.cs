481d67e344d645f6b5f255510672621e7326ab55
LEEINAH <22na7592@gmail.com>
2025-12-18 09:28:48 +0900

    FormStyle

---
+++ 2025-12-18/FormStyle/MainApp.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace FormStyle
+{
+    class MainApp : Form
+    {
+        static void Main(string[] args)
+        {
+            MainApp form = new MainApp();
+
+            form.Width = 400;
+            form.MouseDown += new MouseEventHandler(form_MouseDown);
+
+            Application.Run(form);
+        }
+
+        static void form_MouseDown(object sender, MouseEventArgs e)
+        {
+            Form form = (Form)sender;
+
+            if (e.Button == MouseButtons.Left)
+            {
+                form.MaximizeBox = false;
+                form.MinimizeBox = false;
+                form.Text = "최소화/최대화 버튼이 비활성화 되었습니다.";
+            }
+        }
+    }
+}
